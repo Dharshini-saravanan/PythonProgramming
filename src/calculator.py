71e5f24b8c484150ff9335748f71facb22d8aec7
Dharshini-saravanan <dharshinisaravanan167@gmail.com>
2024-11-10 14:30:44 +0530

    Implemented addtion

--- src/calculator.py
+++ src/calculator.py
@@ -3,3 +3,9 @@ def multipy_numbers(num1, num2):
     return result 
 product= multipy_numbers(4,6)
 print("Product is",product)
+
+def addtion_numbers(num1, num2):
+    result= num1+num2
+    return result 
+addtion= addtion_numbers(9,6)
+print("The Addtion is",addtion)
