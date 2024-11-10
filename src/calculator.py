964af627e9a4866f897937589b44dbb21b91e990
Dharshini-saravanan <dharshinisaravanan167@gmail.com>
2024-11-10 14:22:13 +0530

    Implemented Division

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,10 @@
 def multipy_numbers(num1, num2):
     result= num1*num2
     return result 
+
+def divide_number(num1, num2):
+    result = num1/num2
+    return result
+
 product= multipy_numbers(4,6)
 print("Product is",product)
